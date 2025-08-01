80e134a2b4cb4b1468eaff8f7abd12503998a5df
sunnyycode <sunnya30scr7@gmail.com>
2025-08-01 05:12:43 +0000

    reverse

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int n;
+    char str[100];
 
-    printf("Enter the number of integers: ");
-    scanf("%d", &n);
+    printf("Enter a string: ");
+    // Read a line of text including spaces
+    fgets(str, sizeof(str), stdin);
 
-    int arr[n];
-    int sum = 0;
-
-    printf("Enter %d integers:\n", n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+    // Remove trailing newline if present
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
     }
 
-    double average = (double)sum / n;
-    printf("Average = %.2f\n", average);
+    // Reverse the string
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = temp;
+    }
+
+    printf("Reversed string: %s\n", str);
 
     return 0;
 }
